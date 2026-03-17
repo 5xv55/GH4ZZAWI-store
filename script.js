f06f8eb0ccadefa26e60bf2a5f630d710719b390
5xv55 <mohammedadamghazzawi@gmail.com>
2026-03-16 20:06:54 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-alert("Welcome to GH4ZZAWI Streetwear");
+body{
+background:black;
+color:white;
+font-family:Arial;
+text-align:center;
+}
+
+.shop{
+display:flex;
+justify-content:center;
+gap:40px;
+}
+
+.product{
+border:1px solid white;
+padding:20px;
+}
+
+button{
+padding:10px;
+margin-top:10px;
+cursor:pointer;
+}
